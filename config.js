0a89d0b6b9f346d6347de778e93f462da9725aa5
gestaodoled <gestaodoled@gmail.com>
2026-05-11 15:31:16 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,36 @@
+const CONFIG = {
+
+    width:800,
+    height:30,
+
+    fontSize:12,
+
+    speed:115,
+
+    city:"São Paulo",
+
+    instagram:"@lumix.led",
+
+    title:"LUMIX NEWS",
+
+    enabledBlocks:[
+
+        "clima",
+        "previsao",
+        "transito",
+        "esporte",
+        "resultados",
+        "classificacao",
+        "selecao",
+        "dolar",
+        "euro",
+        "bitcoin",
+        "mercado",
+        "tech",
+        "entretenimento",
+        "news",
+        "instagram"
+
+    ]
+
+};
