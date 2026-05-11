b61f215a5e9d2d937d3f9f1acb7609adebb80aed
gestaodoled <gestaodoled@gmail.com>
2026-05-11 17:10:23 -0300

    Update salamf.js

--- configs/salamf.js
+++ configs/salamf.js
@@ -1,49 +1,22 @@
 const CONFIG = {
 
-    /* RESOLUÇÃO DO RODAPÉ */
-
     width:800,
     height:30,
 
-    /* FONTE */
-
     fontSize:12,
 
-    /* VELOCIDADE */
-
-    speed:170,
-
-    /* CIDADE */
-
-    city:"São Paulo",
-
-    /* INSTAGRAM */
-
-    instagram:"@lumix.led",
-
-    /* TÍTULO */
+    speed:100,
 
     title:"LUMIX NEWS",
 
-    /* BLOCOS */
+    city:"São Paulo",
+
+    instagram:"@lumix.led",
 
     enabledBlocks:[
 
         "clima",
-        "previsao",
-        "transito",
-        "esporte",
-        "resultados",
-        "classificacao",
-        "selecao",
-        "dolar",
-        "euro",
-        "bitcoin",
-        "mercado",
-        "tech",
-        "entretenimento",
-        "news",
-        "instagram"
+        "news"
 
     ]
 
