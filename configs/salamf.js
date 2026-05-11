3e639171c57a149224873da975d416bb66f21fcd
gestaodoled <gestaodoled@gmail.com>
2026-05-11 17:30:54 -0300

    Update salamf.js

--- configs/salamf.js
+++ configs/salamf.js
@@ -1,37 +1,17 @@
 const CONFIG = {
 
-    /* =========================================
-       RESOLUÇÃO
-    ========================================= */
-
     width:800,
     height:30,
 
-    /* =========================================
-       FONTES
-    ========================================= */
-
     fontSize:12,
 
-    /* =========================================
-       VELOCIDADE
-    ========================================= */
-
     speed:170,
-
-    /* =========================================
-       IDENTIDADE
-    ========================================= */
 
     title:"LUMIX NEWS",
 
     city:"São Paulo",
 
     instagram:"@lumix.led",
-
-    /* =========================================
-       BLOCOS ATIVOS
-    ========================================= */
 
     enabledBlocks:[
 
@@ -60,61 +40,6 @@ const CONFIG = {
         "lumix",
         "instagram"
 
-    ],
-
-    /* =========================================
-       RSS ONLINE
-    ========================================= */
-
-    rss:{
-
-        noticias:[
-
-            "https://g1.globo.com/rss/g1/",
-            "https://rss.uol.com.br/feed/noticias.xml"
-
-        ],
-
-        esportes:[
-
-            "https://ge.globo.com/rss/ge/futebol/",
-            "https://www.espn.com.br/rss/futebol.xml"
-
-        ],
-
-        economia:[
-
-            "https://g1.globo.com/rss/g1/economia/",
-            "https://rss.uol.com.br/feed/economia.xml"
-
-        ],
-
-        tecnologia:[
-
-            "https://g1.globo.com/rss/g1/tecnologia/",
-            "https://rss.uol.com.br/feed/tecnologia.xml"
-
-        ],
-
-        entretenimento:[
-
-            "https://g1.globo.com/rss/g1/pop-arte/",
-            "https://rss.uol.com.br/feed/entretenimento.xml"
-
-        ],
-
-        transito:[
-
-            "https://g1.globo.com/rss/g1/sao-paulo/"
-
-        ],
-
-        saude:[
-
-            "https://g1.globo.com/rss/g1/bemestar/"
-
-        ]
-
-    }
+    ]
 
 };
