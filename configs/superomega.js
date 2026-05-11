1166f230c2dbdbb7d615a3959f6bfe788f5d5ddc
gestaodoled <gestaodoled@gmail.com>
2026-05-11 16:39:16 -0300

    Create superomega.js

---
+++ configs/superomega.js
@@ -0,0 +1,50 @@
+const CONFIG = {
+
+    /* RESOLUÇÃO DO RODAPÉ */
+
+    width:1140,
+    height:64,
+
+    /* FONTE */
+
+    fontSize:26,
+
+    /* VELOCIDADE */
+
+    speed:170,
+
+    /* CIDADE */
+
+    city:"São Paulo",
+
+    /* INSTAGRAM */
+
+    instagram:"@lumix.led",
+
+    /* TÍTULO */
+
+    title:"LUMIX NEWS",
+
+    /* BLOCOS */
+
+    enabledBlocks:[
+
+        "clima",
+        "previsao",
+        "transito",
+        "esporte",
+        "resultados",
+        "classificacao",
+        "selecao",
+        "dolar",
+        "euro",
+        "bitcoin",
+        "mercado",
+        "tech",
+        "entretenimento",
+        "news",
+        "instagram"
+
+    ]
+
+};
